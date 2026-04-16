ca6a1b46fea11d2b65b7a5fede8eb2ab6b09d680
sbtbrkcbc <sabitburakcebeci@gmail.com>
2026-04-16 17:38:36 +0100

    Add craco config

---
+++ frontend/craco.config.js
@@ -0,0 +1,28 @@
+frontend/craco.config.js
+İçerik (Emergent özel kısımları temizledim):
+
+// craco.config.js
+const path = require("path");
+
+module.exports = {
+  webpack: {
+    alias: {
+      '@': path.resolve(__dirname, 'src'),
+    },
+    configure: (webpackConfig) => {
+      // Add ignored patterns to reduce watched directories
+      webpackConfig.watchOptions = {
+        ...webpackConfig.watchOptions,
+        ignored: [
+          '**/node_modules/**',
+          '**/.git/**',
+          '**/build/**',
+          '**/dist/**',
+          '**/coverage/**',
+          '**/public/**',
+        ],
+      };
+      return webpackConfig;
+    },
+  },
+};
